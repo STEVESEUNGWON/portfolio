026944b911d0ff5b097c5e837b74ce32ced69cd9
STEVE <yangwony97@gmail.com>
2024-07-03 15:31:24 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+    const sections = document.querySelectorAll(".section");
 
-// JavaScript can be added here if needed
+    function setActiveLink() {
+        let index = sections.length;
+
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove("active"));
+        navLinks[index].classList.add("active");
+    }
+
+    setActiveLink();
+    window.addEventListener("scroll", setActiveLink);
+});
